003c96370861dd829301e4d9162e4189b8dabaeb
Peter Etherington <peter.etherington@skybettingandgaming.com>
2024-02-16 09:54:23 +0000

    Generics with bounds and wildcards

---
+++ src/com/advanced/generics/weirdpart/GenericFunctions.java
@@ -0,0 +1,32 @@
+package com.advanced.generics.weirdpart;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class GenericFunctions<T> {
+
+
+    public static<T> void displayToString(T typeData){
+        System.out.println(typeData.toString());
+    }
+
+    public static void addNumbers(ArrayList<? super Integer> list){
+        for (int i =1; i <= 10; i++){
+            list.add(i);
+        }
+    }
+
+    public static void printList(ArrayList<?> list) {
+        for (Object element : list){
+            System.out.println(element + " ");
+        }
+        System.out.println();
+    }
+
+    public  void printSimpleGenerics(ArrayList<T> list) {
+        for (Object element : list){
+            System.out.println(element + " ");
+        }
+        System.out.println();
+    }
+}
