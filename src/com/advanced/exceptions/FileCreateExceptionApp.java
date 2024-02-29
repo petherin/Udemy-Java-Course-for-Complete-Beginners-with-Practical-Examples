c1f41c92154b445e94f951e1eec1f17b2339c583
Peter Etherington <peter.etherington@skybettingandgaming.com>
2024-02-29 16:12:44 +0000

    Cleaner form of try-catch

--- src/com/advanced/exceptions/FileCreateExceptionApp.java
+++ src/com/advanced/exceptions/FileCreateExceptionApp.java
@@ -6,22 +6,14 @@ import java.io.PrintWriter;
 
 public class FileCreateExceptionApp {
     public static void main(String[] args) {
-createFile("outputfile.txt");
+        createFile("outputfile.txt");
     }
 
-    private static void createFile(String fileName){
-        PrintWriter outputFile = null;
-        try {
-            outputFile = new PrintWriter(new FileWriter(fileName));
+    private static void createFile(String fileName) {
+        try (PrintWriter outputFile = new PrintWriter(new FileWriter(fileName));) {
+            outputFile.println("Hello");
         } catch (IOException e) {
             System.err.println("Caught an IOException: " + e.getMessage());
-        } finally {
-            if (outputFile!= null){
-                outputFile.close();
-                System.out.println("The printWriter is closed.");
-            } else {
-                System.out.println("The printWriter is not open.");
-            }
         }
         System.out.println("This line comes after we tried to create a file.");
     }
